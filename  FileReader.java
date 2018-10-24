b376a37c81c3a1bec5a0b0f5b5d4ebb1a2c67edb
wfwy <1668603052@qq.com>
2018-10-24 15:42:22 +0800

    FileReader读取文本文件

---
+++  FileReader.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) throws IOException {
+        FileReader fr=new FileReader("d:\\3.txt");
+        char[] c=new char[500];
+        int num=fr.read(c);
+        String str=new String(c,0,num);
+        System.out.println("读取的字符个数为："+num+"，其内容如下");
+        System.out.println(str);
+        fr.close();
+
+    }
+}
